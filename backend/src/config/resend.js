43ab77e7503c60bbdecf94671b48d2a491f7c242
Dinakara Ambepitiya <dinakaranadun@gmail.com>
2025-12-04 20:53:17 +0530

    fixes

--- backend/src/config/resend.js
+++ backend/src/config/resend.js
@@ -5,19 +5,13 @@ import { welcomeEmailTemplate } from '../email/welcomeEmailTemplate.js';
 const resend = new Resend(RESEND_API_KEY);
 
 const sendWelcomeEmail = async (email, name, clientUrl) => {
-  try {
+  
     const data = await resend.emails.send({
       from: `${EMAIL_FROM_NAME} <${EMAIL_FROM}>`,
       to: email,
       subject: 'Welcome to NodeTalk',
       html: welcomeEmailTemplate(name, clientUrl),
     });
-
-    console.log(`✅ Welcome email sent to ${email}`, data);
-
-  } catch (error) {
-    console.error(`❌ Failed to send welcome email to ${email}:`, error);
-  }
 };
 
 
